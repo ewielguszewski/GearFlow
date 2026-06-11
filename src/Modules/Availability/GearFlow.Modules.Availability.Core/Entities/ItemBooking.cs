6871525f5a468bdaae19047d3ab33ec1d882b01f
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-06-11 19:31:31 +0200

    feat(availability): introduce core domain with ItemBooking

---
+++ src/Modules/Availability/GearFlow.Modules.Availability.Core/Entities/ItemBooking.cs
@@ -0,0 +1,40 @@
+﻿using GearFlow.Shared.Abstractions.ValueObjects;
+
+namespace GearFlow.Modules.Availability.Core.Entities;
+
+public class ItemBooking
+{
+    public Guid Id { get; private set; }
+    public Guid ItemId { get; private set; }
+    public DateRange TimePeriod { get; private set; }
+    public Guid SourceId { get; private set; }
+    public BookingType Source { get; private set; }
+    
+    private ItemBooking() { } // EF
+
+    private ItemBooking(Guid itemId, DateRange timePeriod, Guid sourceId, BookingType source)
+    {
+        Id = Guid.NewGuid();
+        ItemId = itemId;
+        TimePeriod = timePeriod;
+        SourceId = sourceId;
+        Source = source;
+    }
+
+    public static ItemBooking Create(Guid itemId, DateRange timePeriod, Guid sourceId, BookingType source)
+        => new ItemBooking(itemId, timePeriod, sourceId, source);
+}
+
+
+public enum BookingType
+{
+    Reservation,
+    Maintenance,
+    Rental,
+    ManualBlock
+}
+
+// for now just delete record if not affecting availability anymore (no BookingStatus)
+
+// future: need to block daterange.start or daterange.end on days like sundays and overall maybe some calendar in future to
+// manage when rental office is open? in that case need to have like schedule of how far in the future can be booked
