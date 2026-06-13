1920e731e34efd478d2eedb232c348a0468d4b38
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-06-13 21:26:50 +0200

    fix(catalog): persist variant offer details

--- src/Modules/Catalog/GearFlow.Modules.Catalog.Domain/Entities/EquipmentVariant.cs
+++ src/Modules/Catalog/GearFlow.Modules.Catalog.Domain/Entities/EquipmentVariant.cs
@@ -26,6 +26,9 @@ public class EquipmentVariant
     {
         Id = Guid.NewGuid();
         EquipmentModelId = equipmentModelId;
+        DisplayName = displayName;
+        PublicNote = publicNote;
+        PriceOverride = priceOverride;
         Size = size;
     }
 
