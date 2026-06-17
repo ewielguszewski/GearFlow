1d84b738650ca9190ee8f9a6aa2e872b9e4eeafb
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-06-17 17:45:51 +0200

    feat(availability); add booking persistence configuration

---
+++ src/Modules/Availability/GearFlow.Modules.Availability.Infrastructure/DAL/AvailabilityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GearFlow.Modules.Availability.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GearFlow.Modules.Availability.Infrastructure.DAL;
+
+public sealed class AvailabilityDbContext : DbContext
+{
+    public DbSet<ItemBooking> Bookings { get; set; }
+
+    public AvailabilityDbContext(DbContextOptions<AvailabilityDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("availability");
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
---
+++ src/Modules/Availability/GearFlow.Modules.Availability.Infrastructure/DAL/Configurations/ItemBookingConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using GearFlow.Modules.Availability.Core.Entities;
+using GearFlow.Shared.Infrastructure.Postgres.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GearFlow.Modules.Availability.Infrastructure.DAL.Configurations;
+
+public sealed class ItemBookingConfiguration : IEntityTypeConfiguration<ItemBooking>
+{
+    public void Configure(EntityTypeBuilder<ItemBooking> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.ItemId)
+            .IsRequired();
+        
+        builder.Property(x => x.VariantId)
+            .IsRequired();
+        
+        builder.ComplexProperty(x => x.TimePeriod)
+            .ConfigureDateRange("time_period")
+            .IsRequired();
+        
+        builder.Property(x => x.SourceId)
+            .IsRequired();
+        
+        builder.Property(x => x.Source)
+            .HasConversion<string>()
+            .IsRequired();
+    }
+}
