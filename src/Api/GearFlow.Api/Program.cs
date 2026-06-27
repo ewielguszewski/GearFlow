e622742584314a1bcb76ea41860a5a35eb03f662
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-06-27 23:48:06 +0200

    feat(api): wire module infrastructure

--- src/Api/GearFlow.Api/Program.cs
+++ src/Api/GearFlow.Api/Program.cs
@@ -1,22 +1,33 @@
+using GearFlow.Modules.Availability.Infrastructure;
+using GearFlow.Modules.Catalog.Infrastructure;
+using GearFlow.Modules.Reservations.Application.Commands.CreateDraftReservation;
+using GearFlow.Modules.Reservations.Infrastructure;
+using GearFlow.Shared.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure([
+    typeof(CreateDraftReservationCommand).Assembly
+]);
+builder.Services.AddCatalogModule();
+builder.Services.AddAvailabilityModule();
+builder.Services.AddReservationsModule();
+
 builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+var app = builder.Build();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-        app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 
 app.MapControllers();
 
