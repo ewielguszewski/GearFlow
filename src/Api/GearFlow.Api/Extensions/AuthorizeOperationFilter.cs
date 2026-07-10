2e28b8025e3d88167c6c2901d26175d5e701bb1d
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-07-10 20:21:38 +0200

    feat(api): expose users auth endpoints

---
+++ src/Api/GearFlow.Api/Extensions/AuthorizeOperationFilter.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace GearFlow.Api.Extensions;
+
+public class AuthorizeOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        var methodInfo = context.MethodInfo;
+
+        var hasAuthorizeAttribute = methodInfo.DeclaringType?.GetCustomAttributes(true)
+            .OfType<Microsoft.AspNetCore.Authorization.AuthorizeAttribute>().Any() == true
+            || methodInfo.GetCustomAttributes(true)
+            .OfType<Microsoft.AspNetCore.Authorization.AuthorizeAttribute>().Any();
+
+        var hasAllowAnonymousAttribute = methodInfo.GetCustomAttributes(true)
+            .OfType<Microsoft.AspNetCore.Authorization.AllowAnonymousAttribute>().Any();
+
+        if (!hasAuthorizeAttribute || hasAllowAnonymousAttribute)
+        {
+            return;
+        }
+
+        operation.Security ??= new List<OpenApiSecurityRequirement>();
+
+        operation.Security.Add(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
+    }
+}
