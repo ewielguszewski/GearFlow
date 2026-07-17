2755b44d278769321e069d96c0f86ff76954662c
Ernest Wielguszewski <ernest.wielguszewski@microsoft.wsei.edu.pl>
2026-07-17 19:36:58 +0200

    chore(api): configure Serilog with Seq

--- src/Api/GearFlow.Api/Program.cs
+++ src/Api/GearFlow.Api/Program.cs
@@ -8,49 +8,87 @@ using GearFlow.Modules.Rentals.Application.Commands.StartRentalFromReservation;
 using GearFlow.Modules.Rentals.Infrastructure;
 using GearFlow.Shared.Infrastructure;
 using GearFlow.Shared.Abstractions.Security;
+using Serilog;
+using Serilog.Formatting.Compact;
 using System.Text.Json.Serialization;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console(new RenderedCompactJsonFormatter())
+    .CreateBootstrapLogger();
 
-builder.Services.AddInfrastructure([
-    typeof(CreateDraftReservationCommand).Assembly,
-    typeof(StartRentalFromReservationCommand).Assembly
-]);
-builder.Services.AddUsersCore(builder.Configuration);
-builder.Services.AddCatalogModule();
-builder.Services.AddAvailabilityModule();
-builder.Services.AddReservationsModule();
-builder.Services.AddRentalsModule();
-
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false));
-    });
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwagger();
-
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddScoped<IUserContext, UserContext>();
-
-var app = builder.Build();
-
-app.UseInfrastructure();
-
-if (app.Environment.IsDevelopment())
+try
 {
-   await app.InitializeDevelopmentDatabaseAsync();
+    Log.Information("Starting GearFlow API");
 
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSerilog((services, configuration) =>
+    {
+        configuration
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("Application", "GearFlow.Api")
+            .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+            .WriteTo.Console(new RenderedCompactJsonFormatter());
+
+        var seqServerUrl = builder.Configuration["Seq:ServerUrl"];
+
+        if (!string.IsNullOrWhiteSpace(seqServerUrl))
+            configuration.WriteTo.Seq(seqServerUrl);
+    });
+
+    builder.Services.AddInfrastructure([
+        typeof(CreateDraftReservationCommand).Assembly,
+        typeof(StartRentalFromReservationCommand).Assembly
+    ]);
+    builder.Services.AddUsersCore(builder.Configuration);
+    builder.Services.AddCatalogModule();
+    builder.Services.AddAvailabilityModule();
+    builder.Services.AddReservationsModule();
+    builder.Services.AddRentalsModule();
+
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter(allowIntegerValues: false));
+        });
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwagger();
+
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddScoped<IUserContext, UserContext>();
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.UseInfrastructure();
+
+    if (app.Environment.IsDevelopment())
+    {
+        await app.InitializeDevelopmentDatabaseAsync();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    await app.RunAsync();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    Log.Fatal(exception, "GearFlow API terminated unexpectedly");
+    throw;
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
 
 public partial class Program;
